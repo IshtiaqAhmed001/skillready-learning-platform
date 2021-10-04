16dd0322636dce78ebf4f90673c1cad5dba36e40
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-10-04 13:18:14 +0600

    added cards on home page

---
+++ src/components/Courses/Courses.js
@@ -0,0 +1,28 @@
+import './Courses.css'
+import React from 'react';
+import { Card, CardGroup, Col, Row, Button } from 'react-bootstrap';
+
+const Courses = (props) => {
+    const { id, name, duration, rating, price, description, img } = props.course;
+    return (
+        <div className="card-container mb-5">
+            <Card className="h-100" style={{ width: '20rem' }}>
+                <Card.Img variant="top" src={img} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        <p>{description}</p>
+                        <h6>Duration: {duration} | Rating: {rating}</h6>
+                        <h5>Price: {price}</h5>
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <Button>Learn More</Button>
+                </Card.Footer>
+            </Card>
+
+        </div >
+    );
+};
+
+export default Courses;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Home.css'
 import bannerImg from '../../images/topbanner2.png'
 import { Carousel } from 'react-bootstrap';
+import Courses from '../Courses/Courses';
 const Home = () => {
     const [courses, setCourses] = useState([]);
     useEffect(() => {
@@ -15,11 +16,13 @@ const Home = () => {
         <div>
             <div className="home-page1  d-flex justify-content-around">
                 <div className="mt-5">
-                    <h1 className="text-start mt-5 ">SkillReady <br />
+                    <h1 className="text-start mt-5 ">
+                        <span className="display-4 fw-bold">SkillReady</span>
+                        <br />
                         <span>Your home for building skills online</span>
                     </h1>
                     <br />
-                    <p className="text-start fw-bold">You grow while you learn</p>
+                    <h5 className="text-start fw-bold">You grow while you learn</h5>
                     <p>Build your skills with our latest courses and degrees from top-ranked universities and instructors</p>
                 </div>
                 <div>
@@ -27,10 +30,13 @@ const Home = () => {
                 </div>
             </div >
             <div className="home-page2">
-                <h2>Courses We Offer</h2>
-                {
-                    featuredCourses.map(course => <h2>{course.name}</h2>)
-                }
+                <h1 className="fw-bold my-5">Featured Courses</h1>
+                <div className="d-flex justify-content-around">
+                    {
+                        featuredCourses.map(course => <Courses course={course}></Courses>)
+                    }
+                </div>
+
             </div>
         </div>
     );
