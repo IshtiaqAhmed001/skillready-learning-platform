cfa5eb42d3f0ea33993274ce725bd436b6480c3b
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-10-05 21:42:31 +0600

    edited services,readme

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
 # SkillReady - an online skill development platform
 
-This is an online educational website that offers free and paid skill development courses from top class universities and instructors.
+This is an online educational website that offers free and paid skill development courses from top class universities and instructors. It also has opportunities for those having special skills or knowledge on any topic! They can register as instructors and earn by teaching. This site also offers student visa processing ,Ielts courses and many more.
 
 ## Features
 This website has five pages.
-=> Home
-=> Services
-=> About
-=> Blog
-=> Contact
+• Home 
+• Services 
+• About 
+• Blog 
+• Contact 
 
 ### Project Details
-To build this projet we used react library. we also implemented react router here so the pages of the site loads faster than the regular websites.
-
+To build this projet we used React,which is a JavaScript Library. We also implemented react router here so the pages of the site loads faster than the regular websites.
 
 
 ### Live site link:
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import './Services.css';
 import Courses from '../Courses/Courses';
+import { Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import './Services.css';
 
 const Services = () => {
     const [courses, setCourses] = useState([]);
@@ -17,6 +19,44 @@ const Services = () => {
                 {
                     courses.map(course => <Courses course={course}></Courses>)
                 }
+            </div>
+            <div className="more-services">
+                <h2 className="display-5 fw-bold services-title">Other Services</h2>
+                <div className="more-services-grid mx-auto">
+                    <div className="more-services-item">
+                        <NavLink className="more-service-link" to="bbb">
+                            <h2>Private Tution</h2>
+                        </NavLink>
+
+                    </div>
+                    <div className="more-services-item">
+                        <NavLink className="more-service-link" to="bbb">
+                            <h2>Job Training Club</h2>
+                        </NavLink>
+
+                    </div>
+                    <div className="more-services-item">
+                        <NavLink className="more-service-link" to="bbb">
+                            <h2>Student Visa Processing</h2>
+                        </NavLink>
+
+                    </div>
+                    <div className="more-services-item">
+                        <NavLink className="more-service-link" to="bbb">
+                            <h2>IELTS Crash Course</h2>
+                        </NavLink>
+                    </div>
+                </div>
+            </div>
+            <div className="get-preimum mb-5">
+                <h2 className="display-6 fw-bold services-title">Register as an Instructor </h2>
+                <h4 className="mt-4">
+                    Already skilled ?
+                    <br />
+                    You can teach !
+                </h4>
+                <p className="w-75 mx-auto">Share your knowledge with the world and <strong>earn</strong> by teaching students from all around the globe</p>
+                <Button className="register-btn">Register Now</Button>
             </div>
         </div>
     );
